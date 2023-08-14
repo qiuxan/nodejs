b99aaa2054046f921b86ba038986b49e41db6c83
qiuxan <qiuxan@gmail.com>
2023-08-14 10:44:00 +1000

    routing + custmised header + 404

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
 
 // // blocking synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -11,16 +15,42 @@ const fs = require('fs');
 
 
 // Non-blocking asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log({ data2 });
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log({ data2 });
 
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log({ data3 });
-            fs.writeFile('./txt/final.txt', `${data2}\n\n${data3}`, 'utf-8', err => { });
-            console.log('File has been written 🚀');
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             console.log({ data3 });
+//             fs.writeFile('./txt/final.txt', `${data2}\n\n${data3}`, 'utf-8', err => { });
+//             console.log('File has been written 🚀');
+//         });
+
+//     });
+// });
+// console.log('will read file');
+
+//SERVER
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    // console.log("🚀 ~ file: index.js:33 ~ server ~ req:", req) 
+
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview')
+        res.end('hello from the server! It is over view');
+    else if (pathName === '/product') {
+        res.end('hello from the server! It is prod');
+    } else {
+        res.writeHead(404, {
+            'Content-type': "text-html",
+            'myHeader': 'hellow-world'
+
         });
-
-    });
+        res.end('<h1>page not found</h1>');
+    }
 });
-console.log('will read file');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listening to request on port 8000');
+});
